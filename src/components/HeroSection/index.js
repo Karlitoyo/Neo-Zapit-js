da92431f0919c498d2c3a039b8a3aa48bd8184f1
Karlitoyo <karltimmins@gmail.com>
2021-06-24 19:49:02 +0100

    Added front page and navigation

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import Video from '../../videos/neo-zapit.mp4'
+import { HeroContainer, HeroBg, VideoBg, HeroContent,
+HeroH1, HeroBtnWrapper, HeroP, ArrowForward, ArrowRight} from './HeroElements';
+import {Button} from '../ButtonElement';
+
+const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+
+    return (
+        <HeroContainer id="home">
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>
+                    Neo-Zapit
+                </HeroH1>
+                <HeroP>
+                    Peer to Peer Finance
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="wallet"
+                    onMouseEnter={onHover}
+                    onMpouseLeave={onHover}>
+                        Connect Wallet {hover ?
+                         <ArrowForward /> :
+                          <ArrowRight />}
+                          </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
