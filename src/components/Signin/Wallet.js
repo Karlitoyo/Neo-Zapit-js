aa2d130657761e4a102072190ab95b8e62eb5534
Karlitoyo <karltimmins@gmail.com>
2021-07-19 14:43:26 +0100

    amend wallet connect to standalone clinet

--- src/components/Signin/Wallet.js
+++ src/components/Signin/Wallet.js
@@ -1,73 +1,35 @@
 import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
 
-// Create connector
-const connector = new WalletConnect(
-  {
-    // Required
-    uri: "wc:8a5e5bdc-a0e4-47...TJRNmhWJmoxdFo6UDk2WlhaOyQ5N0U=",
-    // Required
-    clientMeta: {
-      description: "WalletConnect Developer Neo-Zapit App",
-      url: "https://walletconnect.org",
-      icons: ["https://walletconnect.org/walletconnect-logo.png"],
-      name: "WalletConnect",
-    },
-  },
-  {
-    // Optional
-    url: "<YOUR_PUSH_SERVER_URL>",
-    type: "fcm",
-    token: token,
-    peerMeta: true,
-    language: language,
-  }
-);
-
-// Subscribe to session requests
-connector.on("session_request", (error, payload) => {
-  if (error) {
-    throw error;
-  }
-
-  // Handle Session Request
-
-  /* payload:
-  {
-    id: 1,
-    jsonrpc: '2.0'.
-    method: 'session_request',
-    params: [{
-      peerId: '15d8b6a3-15bd-493e-9358-111e3a4e6ee4',
-      peerMeta: {
-        name: "WalletConnect Example",
-        description: "Try out WalletConnect v1.0",
-        icons: ["https://example.walletconnect.org/favicon.ico"],
-        url: "https://example.walletconnect.org"
-      }
-    }]
-  }
-  */
+// Create a connector
+const connector = new WalletConnect({
+  bridge: "https://bridge.walletconnect.org", // Required
+  qrcodeModal: QRCodeModal,
 });
 
-// Subscribe to call requests
-connector.on("call_request", (error, payload) => {
+// Check if connection is already established
+if (!connector.connected) {
+  // create new session
+  connector.createSession();
+}
+
+// Subscribe to connection events
+connector.on("connect", (error, payload) => {
   if (error) {
     throw error;
   }
 
-  // Handle Call Request
+  // Get provided accounts and chainId
+  const { accounts, chainId } = payload.params[0];
+});
 
-  /* payload:
-  {
-    id: 1,
-    jsonrpc: '2.0'.
-    method: 'eth_sign',
-    params: [
-      "0xbc28ea04101f03ea7a94c1379bc3ab32e65e62d3",
-      "My email is john@doe.com - 1537836206101"
-    ]
+connector.on("session_update", (error, payload) => {
+  if (error) {
+    throw error;
   }
-  */
+
+  // Get updated accounts and chainId
+  const { accounts, chainId } = payload.params[0];
 });
 
 connector.on("disconnect", (error, payload) => {
